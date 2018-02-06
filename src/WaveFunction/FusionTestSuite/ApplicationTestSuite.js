d73a0514ae40b65782b221001a004bfb36ee6255
rikky <son.levuthai@gmail.com>
2018-02-06 21:23:24 +0700

    add http test suite first

--- src/WaveFunction/FusionTestSuite/ApplicationTestSuite.js
+++ src/WaveFunction/FusionTestSuite/ApplicationTestSuite.js
@@ -1,29 +1,30 @@
 import FusionTestSuite from "./FusionTestSuite";
 import {HttpKernel, ConsoleKernel} from "Fusion";
+import chai from "chai";
+import chaiHttp from "chai-http";
+
+chai.use(chaiHttp);
 
 /**
  * The application test suite
  */
 export default class ApplicationTestSuite extends FusionTestSuite {
 
+    chai = chai;
+
     async fusionActivated(context) {
         this.httpKernel     = await this.container.make(HttpKernel);
-        this.consoleKernel  = await this.container.make(ConsoleKernel);
     }
 
-    async dispatchHttp(request) {
-
-        this.httpKernel(request);
-    }
-
-    async dispatchConsole(argv) {
-
+    dispatchHttp() {
+        return chai.request(this.httpKernel.callback());
     }
 
     /**
      * @abstract
      */
     manifest() {
+
     }
 
     /**
