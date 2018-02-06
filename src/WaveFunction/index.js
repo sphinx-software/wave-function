2e7155e6502d6abd345966981280a753aa379337
rikky <son.levuthai@gmail.com>
2018-02-06 21:32:12 +0700

    change the module index

---
+++ src/WaveFunction/index.js
@@ -0,0 +1,6 @@
+export Runner from "./Runner";
+export TestSuite from "./TestSuite";
+export Formatter from "./Formatter";
+export FusionTestSuite from "./FusionTestSuite/FusionTestSuite";
+export ApplicationTestSuite from "./FusionTestSuite/ApplicationTestSuite";
+export * from "./decorators";
--- src/index.js
+++
@@ -1,6 +0,0 @@
-export Runner from "./WaveFunction/Runner";
-export TestSuite from "./WaveFunction/TestSuite";
-export Formatter from "./WaveFunction/Formatter";
-export FusionTestSuite from "./WaveFunction/FusionTestSuite/FusionTestSuite";
-export ApplicationTestSuite from "./WaveFunction/FusionTestSuite/ApplicationTestSuite";
-export * from "./WaveFunction/decorators";
